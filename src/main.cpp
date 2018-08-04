a1d7598de717c29efec2793d08817336b490c8ec
duanjhLoad <421098020@qq.com>
2018-08-04 18:05:49 +0800

    多线程交互调用

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,52 @@
 #include "data_struct/data_struct.h"
 #include "iostream"
 #include "algorithm/Dijkstra.h"
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+
+
+mutex mutex1;
+condition_variable condition_var;
+bool flag = true;
+
+void th1_func()
+{
+	std::unique_lock<mutex> unlock(mutex1);
+	while (true)
+	{
+		std::this_thread::sleep_for(std::chrono::seconds(1));
+		condition_var.wait(unlock, [] {return flag; });
+		printf("1\n");
+		flag = false;
+		condition_var.notify_one();
+	}
+
+}
+
+void th2_func()
+{
+	std::unique_lock<mutex> unlock(mutex1);
+	while (true)
+	{
+		std::this_thread::sleep_for(std::chrono::seconds(1));
+		condition_var.wait(unlock, [] {return !flag; });
+		printf("2\n");
+		flag = true;
+		condition_var.notify_one();
+	}
+
+}
+
+void test_thread()
+{
+	std::thread th1(th1_func);
+	std::thread th2(th2_func);
+
+	th1.join();
+	th2.join();
+}
+
 
 int main()
 {
@@ -22,5 +68,8 @@ int main()
 
 	clockwisePrint();
 
+	test_thread();
 	system("pause");
+
+	return 0;
 }
